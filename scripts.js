491e3c46511553b4fdb2c7cc644c7e67bc814c56
volrar <logan.rupp1@gmail.com>
2024-11-03 15:41:37 -0700

    Lots of changes.
    Got the faq according working on the contact page.
    
    reworking the layout of the about page.
    
    reworking the layout of the contact page.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
-document.querySelectorAll('.question').forEach(question => {
-    question.addEventListener('click', () => {
-        const answer = question.nextElementSibling;
-        // Toggle the display style between 'none' and 'block'
-        answer.style.display = answer.style.display === 'none' ? 'block' : 'none';
-    });
+const faqItems = document.querySelectorAll('.question'); // Selects all faq questions and names them 'faqItems'
+
+faqItems.forEach((item) => { //for Each 'item' in faqItems
+  item.addEventListener('click', () => {//add an event listener for the 'click' event.
+    const answer = item.nextElementSibling; //selects the next sibling of question (faq-answer) and set's that to the const answer
+    answer.classList.toggle('visible'); //toggles the visible class for faq-answer (display initiall set to none)
+  });
 });
